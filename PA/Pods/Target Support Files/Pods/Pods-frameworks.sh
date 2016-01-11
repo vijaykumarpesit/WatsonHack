a53a12d1eb83293a5de2f769bc199f4b99f5fc2d
Vijay <vijaykumar.pesit@gmail.com>
2016-01-11 15:07:19 +0530

    Add messaging UI

--- PA/Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ PA/Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -85,7 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Charts.framework"
+  install_framework "Pods/JSQMessagesViewController.framework"
+  install_framework "Pods/JSQSystemSoundPlayer.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Charts.framework"
+  install_framework "Pods/JSQMessagesViewController.framework"
+  install_framework "Pods/JSQSystemSoundPlayer.framework"
 fi
